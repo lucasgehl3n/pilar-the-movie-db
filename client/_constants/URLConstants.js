a2d635a89b522ed1376a500fe38107e2ce106f54
Lucas Gehlen <contato.lucasgehlen@gmail.com>
2024-10-15 22:37:46 -0300

    Fixing URL Constants problem with .env format

--- client/_constants/URLConstants.js
+++ client/_constants/URLConstants.js
@@ -1,6 +1,10 @@
+import { useRuntimeConfig } from '#app';
+
+const config = useRuntimeConfig();
+
 export default {
-    API_MOVIES_LIST: `${process.env.BASE_URL_BACKEND}/api/getMovies`,
-    API_MOVIES_SEARCH: `${process.env.BASE_URL_BACKEND}/api/searchMovies`,
-    API_MOVIE_DETAIL: `${process.env.BASE_URL_BACKEND}/api/detail`,
+    API_MOVIES_LIST: `${config.public.BASE_URL_BACKEND}/api/getMovies`,
+    API_MOVIES_SEARCH: `${config.public.BASE_URL_BACKEND}/api/searchMovies`,
+    API_MOVIE_DETAIL: `${config.public.BASE_URL_BACKEND}/api/detail`,
     URL_IMAGE_PATH: 'https://image.tmdb.org/t/p/',
 }
--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,23 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-      '@nuxtjs/tailwindcss',
-      '@pinia/nuxt',
-      '@nuxt/test-utils/module'
-  ],
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@pinia/nuxt',
+        '@nuxt/test-utils/module',
+    ],
 
-  postcss: {
-      plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-      },
-  },
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 
-  css: [
-      '~/assets/scss/main.scss',
-      'flowbite/dist/flowbite.css',
-  ],
+    css: [
+        '~/assets/scss/main.scss',
+        'flowbite/dist/flowbite.css',
+    ],
 
-  ssr: false,
-  compatibilityDate: '2024-10-14',
+    ssr: false,
+    compatibilityDate: '2024-10-14',
+    runtimeConfig: {
+        public: {
+            BASE_URL_BACKEND: process.env.BASE_URL_BACKEND,
+        }
+    },
 })
